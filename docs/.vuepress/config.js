d7a3c725a3ee6262792502a16a42829310ec0e82
cinyic <cinyic@163.com>
2023-03-12 16:23:30 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,44 @@
 module.exports = {
   base: '/blogs/',
-  title: '成新宇的博客',
+  title: '小憩',
   description: '日复一日 必有精进',
+  // 移动端优化
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
   theme: 'reco',
   themeConfig: {
-    subSidebar:'auto',
-    logo: "/avatar.jpg",
+     // 导航栏左侧logo
+     logo: '/avatar.jpg',
+     // 搜索设置
+     search: true,
+     searchMaxSuggestions: 10,
+     // 自动形成侧边导航及其深度
+     subSidebar: 'auto',
+     sidebarDepth: 1,
+     // 最后更新时间
+     lastUpdated: 'Last Updated',
+     // 项目开始时间
+     startYear: '2023',
+    valineConfig: {
+      appId: 'iHc4UchlyB8iAWZb4cMelsSO-gzGzoHsz',// your appId
+      appKey: '8XyUFrMw1853Dy1yOBamDXZX', // your appKey
+    },
     authorAvatar: "/avatar.jpg",
     type: "blog",
     author: "Cinyi Cheng",
     nav: [
-      { text: "首页", link: "/" },
-      {
-        text: "成新宇的博客",
-        items: [
-          { text: "掘金", link: "https://juejin.cn" },
-          { text: "Github", link: "https://github.com" }
-        ]
-      }
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+      { text: 'GitHub', link: 'https://github.com/cinyic', icon: 'reco-github' }
     ],
     blogConfig: {
       category: {
         location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: "博客", // 默认文案 “分类”
+        text: "category", // 默认文案 “分类”
       },
       tag: {
-        location: 4, // 在导航栏菜单中所占的位置，默认4
+        location: 3, // 在导航栏菜单中所占的位置，默认4
         text: "Tag", // 默认文案 “标签”
       },
       socialLinks: [     // 信息栏展示社交信息
@@ -38,4 +51,14 @@ module.exports = {
       lang: "zh-CN",
     },
   },
+  // 插件配置
+  plugins: [
+    [
+      'permalink-pinyin',
+      {
+        lowercase: true,
+        separator: '-'
+      }
+    ]
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,6 @@ home: true
 bgImage: '/home.webp'
 bgImageStyle:
   {
-    height: "100%",
     boxShadow: "0 15px 18px rgba(0,0,0,0.2)"
   }
 heroImage: "/avatar.jpg"
