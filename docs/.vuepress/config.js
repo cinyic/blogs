183d3367486339af1d78bd2acbe88b82439e764c
cinyic <cinyic@163.com>
2023-03-12 08:04:37 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  base: '/blogs/',
+  title: '成新宇的博客',
+  description: '日复一日 必有精进',
+  theme: 'reco',
+  themeConfig: {
+    subSidebar:'auto',
+    logo: "/avatar.jpg",
+    authorAvatar: "/avatar.jpg",
+    type: "blog",
+    author: "Cinyi Cheng",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "成新宇的博客",
+        items: [
+          { text: "掘金", link: "https://juejin.cn" },
+          { text: "Github", link: "https://github.com" }
+        ]
+      }
+    ],
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: "博客", // 默认文案 “分类”
+      },
+      tag: {
+        location: 4, // 在导航栏菜单中所占的位置，默认4
+        text: "Tag", // 默认文案 “标签”
+      },
+    },
+  },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+home: true
+bgImage: '/home.webp'
+bgImageStyle:
+  {
+    height: "100%",
+    boxShadow: "0 15px 18px rgba(0,0,0,0.2)"
+  }
+heroImage: "/avatar.jpg"
+heroText: Cinyi Cheng
+heroImageStyle:
+  {
+    maxHeight: "288px",
+    display: block,
+    borderRadius: "19% 81% 23% 77% / 44% 57% 43% 56%",
+    boxShadow: "0 15px 18px rgba(0,0,0,0.2)",
+  }
+---
